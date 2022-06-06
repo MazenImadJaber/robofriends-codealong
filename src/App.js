99f8b6c0a12a16f79de1507c68b8ca02285ca459
mazenjaber <mazen.jaber@thoughtworks.com>
2022-06-07 09:59:06 +1000

    add loading text in App

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response =>{
-      return response.json();
-    })
-    .then(users =>{
-      this.setState({ robots: users })
-    })
-    
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users })
+      })
+
   }
 
   onSearchChange = (event) => {
@@ -35,15 +35,19 @@ class App extends Component {
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
 
-        <CardList robots={filteredRobots} />
+          <CardList robots={filteredRobots} />
 
-      </div>
-    );
+        </div>
+      );
+    }
   }
 
 }
