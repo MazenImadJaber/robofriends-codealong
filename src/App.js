c522c03119fbdd0fcfd15ce2028fee93cd5a9348
mazenjaber <mazen.jaber@thoughtworks.com>
2022-06-07 10:10:19 +1000

    make list scccrollable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox'
 import './App.css'
+import Scroll from './Scroll';
 
 
 
@@ -42,9 +43,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-
+          <Scroll>
           <CardList robots={filteredRobots} />
-
+          </Scroll>
         </div>
       );
     }
