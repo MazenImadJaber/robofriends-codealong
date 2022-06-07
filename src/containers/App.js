c67e88cf986c1e7cfabe0f581b8cd7ee72a1f4f7
mazenjaber <mazen.jaber@thoughtworks.com>
2022-06-07 10:24:11 +1000

    refactoring

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll';
 import './App.css'
-import Scroll from './Scroll';
-
 
 
 class App extends Component {
@@ -36,21 +34,19 @@ class App extends Component {
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return (!robots.length) ?
+      <h1>Loading</h1> : (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-          <CardList robots={filteredRobots} />
+            <CardList robots={filteredRobots} />
           </Scroll>
         </div>
       );
-    }
   }
-
 }
+
+
 
 export default App;
